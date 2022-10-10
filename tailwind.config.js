29efc339d6fada9549da429f476eba9951cdf84c
Glecio Santos <glecio.santos@gmail.com>
2022-10-10 11:19:21 -0300

    customizacao

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,24 @@
 module.exports = {
   content: ["./**/*.php", "./src/**/*.js"],
+  theme: {
+    colors: {
+      blue: '#1fb6ff',
+      purple: '#7e5bef',
+      pink: '#ff49db',
+      'glecio-blue': '#1fb6ee'
+    },
+    fontFamily: {
+      objectsans: ['Object Sans', 'sans-serif']
+    },
+    extend: {
+      spacing: {
+        '8xl': '96rem',
+        '9xl': '128rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }  
+  }
+},
   plugins: [require("@tailwindcss/typography")]
 }
